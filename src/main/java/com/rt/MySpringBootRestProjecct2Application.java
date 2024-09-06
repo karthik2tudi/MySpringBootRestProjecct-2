e5186de04d43956f77924798dc56af639977eb77
karthik2tudi <karthik1818t@gmail.com>
2024-09-06 22:22:58 +0530

    files added

--- src/main/java/com/rt/MySpringBootRestProjecct2Application.java
+++ src/main/java/com/rt/MySpringBootRestProjecct2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringBootRestProjecct2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootRestProjecct2Application.class, args);
+		 SpringApplication.run(MySpringBootRestProjecct2Application.class, args);
 		
 	}
 
